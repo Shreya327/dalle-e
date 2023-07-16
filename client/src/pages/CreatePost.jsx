79af8633ab76fc05dc7c1e52f1e81f1b94d34fa9
Shreya Wankhade <2021uec1546@mnit.ac.in>
2023-07-16 23:48:29 +0530

    second commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://dall-e-83da.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ const CreatePost = () => {
 
         try {
           const response = await 
-          fetch('http://localhost:8080/api/v1/post', {
+          fetch('https://dall-e-83da.onrender.com/api/v1/post', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
